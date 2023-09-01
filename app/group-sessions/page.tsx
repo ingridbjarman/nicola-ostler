7ffba5efbd8c00639ea05a16f7975d624fbfc139
Ingrid Bjarman <ingridbjarman@Ingrids-MacBook-Pro.local>
2023-09-01 13:35:13 +0100

    Corrected image path name

--- app/group-sessions/page.tsx
+++ app/group-sessions/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import Image from "next/image";
-import BuggyBootcamp from "../../img/buggyBootcamp.jpeg"
+import BuggyBootcamp from "../../img/buggybootcamp.jpeg"
 
 
 export default function GroupSessions() {
